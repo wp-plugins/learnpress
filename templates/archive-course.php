7ce432df5fb1d41b3999c9d0b036b4b820b030be
halink0803 <halink0803@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-08 07:52:10 +0000

    revert
    git-svn-id: https://plugins.svn.wordpress.org/learnpress/trunk@1176574 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/archive-course.php
@@ -0,0 +1,36 @@
+<?php
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header(); ?>
+<?php //echo __FILE__;?>
+<section id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			// Start the Loop.
+			while ( have_posts() ) : the_post();
+				learn_press_get_template( 'archive-course-content.php' );
+			endwhile;
+
+			// Previous/next page navigation.
+			thim_course_paging_nav();
+		endif;
+		?>
+
+	</main>
+	<!-- .site-main -->
+</section><!-- .content-area -->
+
+<?php get_footer(); ?>
